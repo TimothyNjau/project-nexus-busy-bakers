464c17a55a534ed823c49b6791c9b5a802ee4acb
T1mothy Nj4u <93652350+TimothyNjau@users.noreply.github.com>
2025-09-26 15:35:19 +0300

    feat: update Header component layout and styles; add search functionality and color constants

--- busy-bakers/components/layout/Header.tsx
+++ busy-bakers/components/layout/Header.tsx
@@ -2,37 +2,41 @@ import Link from "next/link";
 import Image from "next/image";
 import BusyBakers from "@/public/assets/icons/busy-bakers.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { $PRIMARYCOLOR, $SECONDARYCOLOR, $TERTIARYCOLOR } from "@/styles/color";
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <div>
-        <div>
-          <Link href={"/"}>
-            {" "}
-            <Image
-              src={BusyBakers}
-              alt="Busy Bakers Logo"
-              width={250}
-              priority
-            />
-          </Link>
-        </div>
-        <nav>
-          <Link href={"/"}>Home</Link>
-          <Link href={"/about"}>About Us</Link>
-          <Link href={"/shop"}>Shop</Link>
-          <Link href={"/blog"}>Blog</Link>
-          <Link href={"/contact"}>Contact Us</Link>
-        </nav>
-        <div>
-          <Link href={"/login"}>
-            <FontAwesomeIcon icon={faUser} className="text-3xl" />
-          </Link>
-        </div>
+    <header className={`border-black border-2 bg-[#272726] p-4 text-white flex flex-col sticky`}>
+      <div id="companyIcon" className="absolute top-5">
+        <Link href={"/"}>
+          {" "}
+          <Image src={BusyBakers} alt="Busy Bakers Logo" width={400} priority />
+        </Link>
       </div>
-      <div></div>
+      <div className="relative left-3/4 w-min m-2">
+        <Link href={"/login"}>
+          <FontAwesomeIcon icon={faUser} size="3x" color={$PRIMARYCOLOR}/>
+        </Link>
+      </div>
+      <nav className="w-3/4 flex justify-evenly m-auto text-3xl py-3">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/about"}>About Us</Link>
+        <Link href={"/shop"}>Shop</Link>
+        <Link href={"/blog"}>Blog</Link>
+        <Link href={"/contact"}>Contact Us</Link>
+      </nav>
+      <div className="w-1/2 m-auto">
+        <input
+          type="search"
+          name="ProductSearch"
+          id="ProductSearch"
+          className="border-[#ffc910] border-2 w-3/4 text-2xl bg-gray-300"
+        />
+        <button type="button" className="">
+          <FontAwesomeIcon icon={faMagnifyingGlass} size="2x"/>
+        </button>
+      </div>
     </header>
   );
 };
---
+++ busy-bakers/styles/color.ts
@@ -0,0 +1,5 @@
+//this contains my document primary colors for use in classnames
+
+export const $PRIMARYCOLOR = "#FFC910";  //GOLDEN YELLOW
+export const $SECONDARYCOLOR = "#272726"; //BLACK
+export const $TERTIARYCOLOR = "#F7F4FF"; //wWHITE 
